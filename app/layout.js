eb64be9d0ea4627d668fa5ecc09df75bdbb6a037
JuliaFSO <juliafso@gmail.com>
2023-09-11 10:59:48 -0400

    Upgraded nextjs version

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,9 @@ import Navbar from './components/navbar/navbar';
 
 export default function RootLayout({ children }) {
   return (
-    <div>
+    <>
       <header><Navbar /></header>
       <main>{children}</main>
-    </div>
+    </>
   )
 }
