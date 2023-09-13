b93fbd4898ad270853f01bb7033e7f44a97ee232
JuliaFSO <juliafso@gmail.com>
2023-09-13 14:39:03 -0400

    Downgrade nextjs to 12.3.2-canary.43 version

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import Navbar from './components/navbar/navbar';
+import '../app/components/about/about.module.css';
+import '../app/components/contact/contact.module.css';
+import '../app/components/projects/projects.module.css';
 
 export default function RootLayout({ children }) {
   return (
